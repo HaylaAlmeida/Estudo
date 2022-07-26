a977eb47eb84caa67d9d0938a52b71aa4d56d63c
HaylaAlmeida <80348569+HaylaAlmeida@users.noreply.github.com>
2022-07-26 02:32:41 -0300

    Create CalculadoraIMC.java

---
+++ Fazendo_a_diferença/CalculadoraIMC.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class CalculadoraIMC {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double peso, altura, imc;
+        peso = input.nextDouble();
+        altura = input.nextDouble();
+        
+    }
+}
