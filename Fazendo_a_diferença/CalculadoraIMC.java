b20a3d78152124c002229fe8891bb3bbbc6aa063
HaylaAlmeida <80348569+HaylaAlmeida@users.noreply.github.com>
2022-07-26 12:57:25 -0300

    Update CalculadoraIMC.java

--- Fazendo_a_diferença/CalculadoraIMC.java
+++ Fazendo_a_diferença/CalculadoraIMC.java
@@ -6,6 +6,20 @@ public class CalculadoraIMC {
         double peso, altura, imc;
         peso = input.nextDouble();
         altura = input.nextDouble();
-        
+        imc = peso / (altura * altura);
+        System.out.printf("Seu IMC é %.1f\n", imc);
+        if (imc < 18.5) {
+            System.out.println("Abaixo do peso");
+        }
+        else if (imc >= 18.5 && imc <= 24.9) {
+            System.out.println("Normal");
+        }
+        else if (imc >= 25 && imc <= 29.9){
+            System.out.println("Acima do peso");
+        }
+        else if (imc >= 30) {
+            System.out.println("Obeso");
+        }
+
     }
 }
